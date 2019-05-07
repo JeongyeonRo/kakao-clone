30bde1eec3164c7502ee8fdc69a8307ebdd3eefd
JeongyeonRo <47593108+JeongyeonRo@users.noreply.github.com>
2019-05-07 19:43:44 +0900

    Update
    Update 'more'

---
+++ js/status-bar__clock.js
@@ -0,0 +1,12 @@
+const clockContainer = document.querySelector(".js-clock"),
+clockTitle = clockContainer.querySelector("h1");
+function getTime(){
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    clockTitle.innerText = `${hours<10?`0${hours}`:hours}:${minutes<10`0${minutes}`:'minutes}`}`
+    `${hours < 10 ? `0${hours}` : hours}:${
+        minutes < 10 ? `0${minutes}` : minutes
+      }`;
+    }
+}
